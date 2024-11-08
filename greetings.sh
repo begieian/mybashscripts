3c11ecf60a66acbc41850460b248b63d8b70bccb
begieian <begieianrivera@gmail.com>
2024-11-09 03:38:51 +0800

    this is my first bash script just for greeting.

---
+++ greetings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+########################################
+# Author: Begie Ian Rivera             #
+#                                      #
+# Context: This is to greet the user   #
+# ######################################
+#
+#
+
+echo "Well, hello there $(whoami)"
+echo "You are currently in the host - $(hostname)"
